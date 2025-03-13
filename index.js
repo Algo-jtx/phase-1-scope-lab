7906af747be1fd3c3a006534e1d7a45980425b12
juanita mumbi <mumbijuanita@gmail.com>
2025-03-13 21:51:15 +0300

    scope completed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob"; 
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+  throw new Error("Assignment to constant variable.");
+}
+
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiply) {
+  return function (fare) {
+    return fare * multiply;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverFunction) {
+  return driverFunction(drivers);
+}
